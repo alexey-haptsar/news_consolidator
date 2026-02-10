7159d3d56f2641b231a597a9071b8b22f2a08729
alexeyhaptsarworkflow416 <alexey.haptsar.workflow416@gmail.com>
2026-02-10 15:05:51 +0300

    initial_commit

---
+++ NewsConsolidator/NewsConsolidator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NewsConsolidator
+//
+//  Created by Alexey Haptsar on 10.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
